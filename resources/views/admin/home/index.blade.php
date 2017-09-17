e2f45785cbca87b78aae9ae37f7c7878334ac4dc
Kaishiyoku <dmp@nfze.de>
2017-09-17 16:55:08 +0200

    removed no longer used blade templates

--- resources/views/admin/home/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('admin.layouts.app')
-
-@section('title', trans('home.admin.index.title'))
-
-@section('content')
-    <h1>{{ trans('home.admin.index.title') }}</h1>
-@endsection
--- resources/views/feed/_more_unread.blade.php
+++
@@ -1,3 +0,0 @@
-@foreach ($unreadFeedItems->get() as $feedItem)
-    @include('feed._item', ['feedItem' => $feedItem, 'showActions' => true])
-@endforeach
