d1b72a11004f1dba543737f9659fb939222e0ff8
Kaishiyoku <dmp@nfze.de>
2018-11-14 21:03:11 +0100

    CRY-46 closed #49 added link to Laravel Horizon

--- app/Http/Middleware/Menus.php
+++ app/Http/Middleware/Menus.php
@@ -38,45 +38,43 @@ class Menus
     {
         Menu::setConfig(Config::forBootstrap4());
 
-        if ($this->auth->check()) {
-            Menu::registerDefault([
-                Menu::link('feed.index', '<i class="fa fa-rss" aria-hidden="true"></i> ' . '<span class="d-lg-none d-xl-inline">' . __('common.nav.feed') . '</span>', [], [], ['feed.category']),
-                Menu::link('feed.search_show', '<i class="fa fa-search" ariad-hidden="true"></i> ' . '<span class="d-lg-none d-xl-inline">' . __('common.nav.search') . '</span>', [], [], ['feed.search_result'])
-            ], ['class' => 'navbar-nav mr-auto']);
-        } else {
-            Menu::registerDefault([
-                Menu::link('home.index', '<i class="fa fa-home" aria-hidden="true"></i> ' . '<span class="d-lg-none d-xl-inline">' . __('common.nav.landing_page') . '</span>')
-            ], ['class' => 'navbar-nav mr-auto']);
-        }
+        $isLoggedIn = $this->auth->check();
+        $isAdministrator = $isLoggedIn && $this->auth->user()->is_administrator;
 
-        if ($this->auth->check()) {
+        $adminLinks = $isAdministrator ? [
+            Menu::dropdownHeader(__('common.nav.administration')),
+            Menu::link('/horizon', '<i class="fa fa-compass" aria-hidden="true"></i> ' . __('common.nav.horizon')),
+        ] : [];
+
+        Menu::setConfig(Config::forBootstrap4());
+
+        if ($isLoggedIn) {
+            Menu::registerDefault([
+                Menu::linkRoute('feed.index', '<i class="fa fa-rss" aria-hidden="true"></i> ' . '<span class="d-lg-none d-xl-inline">' . __('common.nav.feed') . '</span>', [], [], ['feed.category']),
+                Menu::linkRoute('feed.search_show', '<i class="fa fa-search" ariad-hidden="true"></i> ' . '<span class="d-lg-none d-xl-inline">' . __('common.nav.search') . '</span>', [], [], ['feed.search_result'])
+            ], ['class' => 'navbar-nav mr-auto']);
+
             Menu::register('user', [
-                Menu::dropdown([
+                Menu::dropdown(array_merge([
                     Menu::dropdownHeader(__('common.nav.general')),
-                    Menu::link('feed.history', '<i class="fa fa-history" aria-hidden="true"></i> ' . __('common.nav.history')),
-                    Menu::link('profile.index', '<i class="fa fa-user" aria-hidden="true"></i> ' . __('common.nav.profile'), [], [], ['profile.edit_password', 'profile.edit_email', 'profile.confirm_new_email']),
+                    Menu::linkRoute('feed.history', '<i class="fa fa-history" aria-hidden="true"></i> ' . __('common.nav.history')),
+                    Menu::linkRoute('profile.index', '<i class="fa fa-user" aria-hidden="true"></i> ' . __('common.nav.profile'), [], [], ['profile.edit_password', 'profile.edit_email', 'profile.confirm_new_email']),
                     Menu::dropdownHeader(__('common.nav.manage')),
-                    Menu::link('feed.manage.index', '<i class="fa fa-rss" aria-hidden="true"></i> ' . __('common.nav.feed'), [], [], ['feed.manage.create', 'feed.manage.edit']),
-                    Menu::link('categories.index', '<i class="fa fa-folder" aria-hidden="true"></i> ' . __('common.nav.categories'), [], [], ['categories.create', 'categories.edit']),
+                    Menu::linkRoute('feed.manage.index', '<i class="fa fa-rss" aria-hidden="true"></i> ' . __('common.nav.feed'), [], [], ['feed.manage.create', 'feed.manage.edit']),
+                    Menu::linkRoute('categories.index', '<i class="fa fa-folder" aria-hidden="true"></i> ' . __('common.nav.categories'), [], [], ['categories.create', 'categories.edit']),
+                ], $adminLinks, [
                     Menu::dropdownDivider(),
-                    Menu::link('logout', '<i class="fa fa-sign-out" aria-hidden="true"></i> ' . __('common.nav.logout'), [], ['data-click' => '#logout-form'])
-
-                ], '<i class="fa fa-user" aria-hidden="true"></i> ' . $this->auth->user()->name . ' ', null, [], ['class' => 'dropdown-menu-right'])
+                    Menu::linkRoute('logout', '<i class="fa fa-sign-out" aria-hidden="true"></i> ' . __('common.nav.logout'), [], ['data-click' => '#logout-form']),
+                ]), '<i class="fa fa-user" aria-hidden="true"></i> ' . $this->auth->user()->name . ' ', null, [], ['class' => 'dropdown-menu-right'])
             ], ['class' => 'navbar-nav']);
         } else {
-            Menu::register('user', [
-                Menu::link('login', '<i class="fa fa-sign-in" aria-hidden="true"></i> ' . __('common.nav.login')),
-                Menu::link('register', '<i class="fa fa-user-plus" aria-hidden="true"></i> ' . __('common.nav.register'))
-            ], ['class' => 'navbar-nav']);
-        }
-
-        if ($this->auth->check() && $this->auth->user()->is_administrator) {
-            Menu::register('administration', [
-
+            Menu::registerDefault([
+                Menu::linkRoute('home.index', '<i class="fa fa-home" aria-hidden="true"></i> ' . '<span class="d-lg-none d-xl-inline">' . __('common.nav.landing_page') . '</span>')
             ], ['class' => 'navbar-nav mr-auto']);
 
-            Menu::register('administration-user', [
-
+            Menu::register('user', [
+                Menu::linRoutek('login', '<i class="fa fa-sign-in" aria-hidden="true"></i> ' . __('common.nav.login')),
+                Menu::linkRoute('register', '<i class="fa fa-user-plus" aria-hidden="true"></i> ' . __('common.nav.register'))
             ], ['class' => 'navbar-nav']);
         }
 
