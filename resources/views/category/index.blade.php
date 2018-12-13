a65c5c2f08fbbaf02a7a7ccb1e0e794bb0634e74
Kaishiyoku <dmp@nfze.de>
2018-12-13 13:57:36 +0100

    made some tables responsive

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -15,27 +15,29 @@
     @if ($categories->count() == 0)
         <p class="lead"><i>@lang('category.index.no_items')</i></p>
     @else
-        <table class="table table-striped" data-provide="tablesorter">
-            <thead>
-            <tr>
-                <th>@lang('validation.attributes.title')</th>
-                <th>@lang('category.index.number_of_feeds')</th>
-                <th class="sorter-false"></th>
-                <th class="sorter-false"></th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($categories->get() as $category)
+        <div class="table-responsive">
+            <table class="table table-striped" data-provide="tablesorter">
+                <thead>
                 <tr>
-                    <td>{{ $category->title }}</td>
-                    <td>{{ $category->feeds->count() }}</td>
-                    <td>
-                        @include('shared._delete_link', ['route' => ['categories.destroy', $category->id]])
-                    </td>
-                    <td>{{ Html::linkRoute('categories.edit', __('common.edit'), [$category->id]) }}</td>
+                    <th>@lang('validation.attributes.title')</th>
+                    <th>@lang('category.index.number_of_feeds')</th>
+                    <th class="sorter-false"></th>
+                    <th class="sorter-false"></th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                @foreach ($categories->get() as $category)
+                    <tr>
+                        <td>{{ $category->title }}</td>
+                        <td>{{ $category->feeds->count() }}</td>
+                        <td>
+                            @include('shared._delete_link', ['route' => ['categories.destroy', $category->id]])
+                        </td>
+                        <td>{{ Html::linkRoute('categories.edit', __('common.edit'), [$category->id]) }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     @endif
 @endsection
--- resources/views/feed_manager/index.blade.php
+++ resources/views/feed_manager/index.blade.php
@@ -17,31 +17,33 @@
             <i>@lang('feed_manager.index.no_feeds_yet')</i>
         </p>
     @else
-        <table class="table table-striped" data-provide="tablesorter">
-            <thead>
-            <tr>
-                <th>@lang('validation.attributes.title')</th>
-                <th>@lang('validation.attributes.category_id')</th>
-                <th>@lang('validation.attributes.is_enabled')</th>
-                <th>@lang('validation.attributes.last_checked_at')</th>
-                <th class="sorter-false"></th>
-                <th class="sorter-false"></th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($feeds->get() as $feed)
-                <tr class="{{ $feed->is_enabled ? '' : 'table-warning' }}">
-                    <td>{{ $feed->title }}</td>
-                    <td>{{ $feed->category->title }}</td>
-                    <td>{{ formatBoolean($feed->is_enabled) }}</td>
-                    <td>{{ $feed->last_checked_at->format(l(DATETIME)) }}</td>
-                    <td>
-                        @include('shared._delete_link', ['route' => ['feed.manage.destroy', $feed->id]])
-                    </td>
-                    <td>{{ Html::linkRoute('feed.manage.edit', __('common.edit'), [$feed->id]) }}</td>
+        <div class="table-responsive">
+            <table class="table table-striped" data-provide="tablesorter">
+                <thead>
+                <tr>
+                    <th>@lang('validation.attributes.title')</th>
+                    <th>@lang('validation.attributes.category_id')</th>
+                    <th width="7%">@lang('validation.attributes.is_enabled')</th>
+                    <th>@lang('validation.attributes.last_checked_at')</th>
+                    <th class="sorter-false"></th>
+                    <th class="sorter-false"></th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                @foreach ($feeds->get() as $feed)
+                    <tr class="{{ $feed->is_enabled ? '' : 'table-warning' }}">
+                        <td>{{ $feed->title }}</td>
+                        <td>{{ $feed->category->title }}</td>
+                        <td>{{ formatBoolean($feed->is_enabled) }}</td>
+                        <td>{{ $feed->last_checked_at->format(l(DATETIME)) }}</td>
+                        <td>
+                            @include('shared._delete_link', ['route' => ['feed.manage.destroy', $feed->id]])
+                        </td>
+                        <td>{{ Html::linkRoute('feed.manage.edit', __('common.edit'), [$feed->id]) }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     @endif
 @endsection
