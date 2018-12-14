e873b379754a399bedd067119ce14fb3ff212893
Kaishiyoku <dmp@nfze.de>
2018-12-14 14:47:55 +0100

    CRY-70 #74 optimized code

--- app/Models/Traits/RelationshipsTrait.php
+++ app/Models/Traits/RelationshipsTrait.php
@@ -12,25 +12,30 @@ trait RelationshipsTrait
     public function relationships()
     {
         $model = new static;
-        $methods = collect((new ReflectionClass($model))->getMethods(ReflectionMethod::IS_PUBLIC));
+
+        try {
+            $methods = collect((new ReflectionClass($model))->getMethods(ReflectionMethod::IS_PUBLIC));
+        } catch (\ReflectionException $e) {
+            $methods = collect();
+        }
 
         $relationships = $methods->filter(function (ReflectionMethod $method) use ($model) {
             return $method->class == get_class($model) && empty($method->getParameters()) && $method->getName() != __FUNCTION__;
-        })->mapWithKeys(function (ReflectionMethod $method) use ($model) {
-
+        })->filter(function ($method) use ($model) {
             try {
-                $return = $method->invoke($model);
-
-                if ($return instanceof Relation) {
-                    return [
-                        $method->getName() => [
-                            'type' => (new ReflectionClass($return))->getShortName(),
-                            'model' => (new ReflectionClass($return->getRelated()))->getName()
-                        ]
-                    ];
-                }
+                return $method->invoke($model) instanceof Relation;
             } catch (ErrorException $e) {
+                return false;
             }
+        })->mapWithKeys(function (ReflectionMethod $method) use ($model) {
+            $returnObj = $method->invoke($model);
+
+            return [
+                $method->getName() => [
+                    'type' => (new ReflectionClass($returnObj))->getShortName(),
+                    'model' => (new ReflectionClass($returnObj->getRelated()))->getName()
+                ]
+            ];
         });
 
         return $relationships->toArray();
