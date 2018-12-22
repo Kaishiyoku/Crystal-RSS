6538b223be7685fba06108f2195c35945bc4f2c0
Kaishiyoku <dmp@nfze.de>
2018-12-22 16:50:33 +0100

    CRY-81 #85 adjusted favicon

--- resources/views/shared/_favicon.blade.php
+++ resources/views/shared/_favicon.blade.php
@@ -3,5 +3,5 @@
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/img/favicon/favicon-16x16.png') }}">
 <link rel="manifest" href="{{ asset('/img/favicon/site.webmanifest') }}">
 <link rel="mask-icon" href="{{ asset('/img/favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="theme-color" content="#19aad1">
+<meta name="msapplication-TileColor" content="#07c0e0">
+<meta name="theme-color" content="#07c0e0">
