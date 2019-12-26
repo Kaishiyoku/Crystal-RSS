0cf1ac7edc6d0abe50d20099ba00130a73b138f4
Kaishiyoku <dmp@nfze.de>
2019-12-26 16:39:53 +0100

    fixed put request function

--- resources/js/core/request/request.js
+++ resources/js/core/request/request.js
@@ -8,7 +8,7 @@ const get = (url) => axios.get(url, {headers});
 
 const post = (url, data) => axios.post(url, data, {headers});
 
-const put = (url, data) => axios.post(url, data, {headers});
+const put = (url, data) => axios.put(url, data, {headers});
 
 export {
     get,
