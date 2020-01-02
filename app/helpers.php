c5b30c32c55b509a72aeba4658fde052267a2f2b
Kaishiyoku <dmp@nfze.de>
2020-01-02 20:09:58 +0100

    refactored helper function code

--- app/helpers.php
+++ app/helpers.php
@@ -1,28 +1,31 @@
 <?php
 
+use App\Models\Category;
 use App\Models\FeedItem;
+use App\Models\FeedItemCategory;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
+
 const DATETIME = 'datetime';
 const DATE = 'date';
 const DATE_WITH_DAY_OF_WEEK = 'date_with_day_of_week';
 
 if (! function_exists('l')) {
-    function l($type) {
+    function l(string $type): string {
         return __('common.date_formats.' . $type);
     }
 }
 
 if (! function_exists('getYearsFrom')) {
-    function getYearsFrom($year)
+    function getYearsFrom(int $year): string
     {
         return $year < date('Y') ? $year . '-' . date('Y') : date('Y');
     }
 }
 
 if (! function_exists('getUnreadFeedItemCountForCategory')) {
-    function getUnreadFeedItemCountForCategory($category)
+    function getUnreadFeedItemCountForCategory(Category $category): int
     {
         return $category->feeds()->withCount(['feedItems' => function ($query) {
             return $query->unread();
@@ -31,9 +34,9 @@ if (! function_exists('getUnreadFeedItemCountForCategory')) {
 }
 
 if (! function_exists('formatBoolean')) {
-    function formatBoolean($bool)
+    function formatBoolean(bool $bool): string
     {
-        if ($bool == true || $bool == 1) {
+        if ($bool === true || $bool === 1) {
             $str = __('common.lists.boolean.1');
         } else {
             $str = __('common.lists.boolean.0');
@@ -43,29 +46,28 @@ if (! function_exists('formatBoolean')) {
     }
 }
 
-
 if (! function_exists('upper')) {
-    function upper($string) {
-        return \Illuminate\Support\Str::upper($string);
+    function upper(string $string): string {
+        return Str::upper($string);
     }
 }
 
 if (! function_exists('itemIf')) {
-    function itemIf($item, $isVisible, $default = null) {
+    function itemIf($item, bool $isVisible, $default = null) {
         return $isVisible ? $item : $default;
     }
 }
 
 if (! function_exists('removeNulls')) {
-    function removeNulls(array $arr) {
+    function removeNulls(array $arr): array {
         return array_filter($arr, function ($item) {
-            return $item != null;
+            return $item !== null;
         });
     }
 }
 
 if (! function_exists('syncFeedItemCategories')) {
-    function syncFeedItemCategories(Collection $categoryTitles, User $user, FeedItem $feedItem)
+    function syncFeedItemCategories(Collection $categoryTitles, User $user, FeedItem $feedItem): void
     {
         $categoryIds = [];
 
@@ -73,8 +75,8 @@ if (! function_exists('syncFeedItemCategories')) {
             $currentCategory = $user->feedItemCategories()->whereTitle($categoryTitle)->first();
 
             // if category currently doesn't exist, create it
-            if ($currentCategory == null) {
-                $newCategory = new \App\Models\FeedItemCategory();
+            if ($currentCategory === null) {
+                $newCategory = new FeedItemCategory();
                 $newCategory->title = $categoryTitle;
 
                 $newCategory->user_id = $user->id;
@@ -97,7 +99,7 @@ if (! function_exists('createDateFromStr')) {
      * @param string $str
      * @return Carbon|null
      */
-    function createDateFromStr($str): ?Carbon
+    function createDateFromStr(string $str): ?Carbon
     {
         try {
             return Carbon::createFromFormat(__('common.date_formats.date'), $str)->startOfDay();
