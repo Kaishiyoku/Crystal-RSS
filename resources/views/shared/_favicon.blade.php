74428c299bf8d164b5d0ae7e1371ba430ad46bc6
Kaishiyoku <dmp@nfze.de>
2020-10-05 20:58:31 +0200

    adjusted mobile browser theme colors

--- resources/views/shared/_favicon.blade.php
+++ resources/views/shared/_favicon.blade.php
@@ -2,6 +2,6 @@
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/img/favicon/favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/img/favicon/favicon-16x16.png') }}">
 <link rel="manifest" href="{{ asset('/img/favicon/site.webmanifest') }}">
-<link rel="mask-icon" href="{{ asset('/img/favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
-<meta name="msapplication-TileColor" content="#07c0e0">
-<meta name="theme-color" content="#07c0e0">
+<link rel="mask-icon" href="{{ asset('/img/favicon/safari-pinned-tab.svg') }}" color="#6927ff">
+<meta name="msapplication-TileColor" content="#6927ff">
+<meta name="theme-color" content="#6927ff">
